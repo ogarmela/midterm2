2bd45cd60efe23e6de580b4efa11d31e1ddf0e0e
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-06-19 16:33:25 -0700

    Add files via upload

---
+++ maryam.java
@@ -0,0 +1,26 @@
+
+public class maryam {
+	public static void main(String[] args) 
+    { 
+        String str = "Julia Lucas Mia -1"; 
+        int n = str.length(); 
+        Permutation permutation = new Permutation(); 
+        permutation.permute(str, 0, n-1); 
+    } 
+}
+
+private void permute(String str, int l, int r) 
+{ 
+    if (l == r) 
+        System.out.println(str); 
+    else
+    { 
+        for (int i = l; i <= r; i++) 
+        { 
+            str = swap(str,l,i); 
+            permute(str, l+1, r); 
+            str = swap(str,l,i); 
+        } 
+    } 
+
+}
